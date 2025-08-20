6a94fd4fc6a98eed67928a4d7672babce16c7abf
ShenalSen <shenal.unc555@gmail.com>
2025-08-21 02:40:38 +0530

    Update database configuration to use environment variables for host, user, password, and database

--- FlaskApp/config/db_config.py
+++ FlaskApp/config/db_config.py
@@ -1,6 +1,8 @@
+import os
+
 DB_CONFIG = {
-    'host': 'localhost',
-    'user': 'admin',
-    'password': 'password',
-    'database': 'mylistdb'
+    "host": os.getenv("DB_HOST"),       # Will be 'mysql-db'
+    "user": os.getenv("DB_USER"),       # Will be 'root'
+    "password": os.getenv("DB_PASSWORD"), # Will be 'password'
+    "database": os.getenv("DB_DATABASE")  # Will be 'webapp'
 }
